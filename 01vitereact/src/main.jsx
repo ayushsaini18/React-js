afb7a880b2e986faba2d208a90466368f91b0c51
ayushsaini18 <100209722+ayushsaini18@users.noreply.github.com>
2024-01-21 13:13:50 +0530

    updated

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anothertElement=(
+  <a href="https://www.google.com" target='_blank'>Visit Me</a>
+)
+const user="Mega and React"
+const reactElement=React.createElement(
+  'a',
+  {href:"https://www.google.com",target:"_blank"},
+  "click me to visit google",<br/>,
+  user
 
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  //anothertElement
+  reactElement
+  
+
 )
